3d9fd55a32be612674da5d0c7f617e9d389c4e5f
Jonathan López <71607470+SI727367@users.noreply.github.com>
2020-11-25 19:49:01 -0600

    Modular

--- Montgomery_Power.py
+++ Montgomery_Power.py
@@ -1,20 +1,18 @@
+A = int(input("Ingrese el primer numero\n ~>"))
+B = int(input("Ingrese el segundo numero\n ~>"))
+N = int(input("Ingrese el modulo\n ~>"))
 
-def power(x, y, p) : 
-    resultado = 1
-    x = x % p  
-    if (x == 0): 
-        return 0
-    while (y > 0) : 
-        if ((y & 1) == 1) : 
-            resultado = (resultado * x) % p 
-        y = y >> 1 
-        x = (x ** 2) % p 
-          
-    return resultado 
-      
-  
-  
-x = int(input("Ingrese el primer numero\n ~>"))
-y = int(input("Ingrese el segundo numero\n ~>"))
-p = int(input("Ingrese el modulo\n ~>"))
-print("PExponente es ", power(x, y, p)) 
+R = 1
+A = A % N 
+    
+if (x == 0) : 
+    return 0
+
+while (B > 0) :    
+    if ((B & 1) == 1) : 
+        R = (R * A) % N
+
+    B = B >> 1 
+    A = (A * A) % N
+
+print(R)
